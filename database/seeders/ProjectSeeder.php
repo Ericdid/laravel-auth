d4a137d37ef1489cb10746aa7bc8610253eab569
Ericdid <ericdidom@gmail.com>
2023-10-31 18:09:34 +0100

    add.seeder-table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +16,15 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->word();
+            $project->content = $faker->$faker->paragraphs(2, true);
+            $project->slug = str::slug($project->title);
+
+        }
+
     }
 }
