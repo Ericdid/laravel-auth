4216c96e9d42050e87f19488c6e8755afcc40f79
Ericdid <ericdidom@gmail.com>
2023-10-31 20:43:10 +0100

    add.pagination

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = project::all();
+        $projects = project::paginate(5);
         return view('admin.projects.index', compact('projects'));
     }
 
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,5 +3,7 @@
 @section('content')
     <div class="container mt-3">
         @dump($projects)
+
+        {{ $projects->links('pagination::bootstrap-5') }}
     </div>
 @endsection
